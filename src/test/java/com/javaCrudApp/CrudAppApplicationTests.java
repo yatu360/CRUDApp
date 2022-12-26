b186bd95838f3ab676ee5892b177ce60b7d21627
yatu360 <yatu360@gmail.com>
2022-12-26 11:20:59 +0000

    initial commit

---
+++ src/test/java/com/javaCrudApp/CrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaCrudApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
